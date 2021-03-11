31754ba5cc6f11fd5da6823d1b6f19229a04f6f6
aiwaverse <aiwavision@protonmail.com>
2021-03-11 09:22:42 -0300

    removed fill_info, changed some logic

--- pymusicaltext/core/action.py
+++ pymusicaltext/core/action.py
@@ -4,27 +4,28 @@ from mido import MetaMessage
 
 
 class Action:
-    def __init__(self, act: str) -> None:
+    def __init__(
+        self, act: str, volume: int, octave: int, bpm: int, instrument: int
+    ) -> None:
         self.__action = self.__decode_action(act)
-
-    def fill_info(self, volume, octave, bpm, instrument):
-        """
-        this funciton should be called when "running" the action, and it takes the current player info
-        """
         self.__volume = volume
         self.__octave = octave
         self.__bpm = bpm
         self.__instrument = instrument
 
-    def execute(self) -> Tuple[int, int, int, int]:
+    def execute(self, meta_message: List[MetaMessage]) -> Tuple[int, int, int, int]:
         """
         executes the action that the instance represents
         return a tuple with (volume, octave, bpm, instrument)
         with possible modifications
+        the meta_message argument should be an empty list (but named)
         """
         to_run = self.__decode_action(self.__action)
         to_run()
         return (self.__volume, self.__octave, self.__bpm, self.__instrument)
+
+    # TODO: come up with a way to check if meta_message needs to be written
+    # and change it accordingly
 
     def __decode_action(self, act):
         raise NotImplementedError("TODO")
@@ -51,7 +52,7 @@ class Action:
     def __increase_volume(self) -> None:
         # the volume will NOT be duplicated for now, this would cause the volume to be only at two states
         # this increases it by 10%
-        self.__volume *= 1.1
+        self.__volume = round(self.__volume * 1.1)
         if self.__volume > 128:
             self.__volume = 128
 
@@ -62,3 +63,15 @@ class Action:
     def __change_instrument(self) -> None:
         # a random instrument from 0 to 127, as midi standard
         self.__instrument = choice(range(0, 128))
+
+    def __write_instrument_message(self, message):
+        """
+        writes a MetaMessage on message that changes the instrument
+        """
+        raise NotImplementedError("TODO")
+
+    def __write_bpm_message(self, message):
+        """
+        writes a MetaMessage on message that changes the bpm (tempo)
+        """
+        raise NotImplementedError("TODO")
