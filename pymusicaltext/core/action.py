cba1ff46310d5e819305a63856ee96a658906b3e
aiwaverse <aiwavision@protonmail.com>
2021-04-10 16:52:24 -0300

    Rewrote decode and execute

--- pymusicaltext/core/action.py
+++ pymusicaltext/core/action.py
@@ -1,13 +1,20 @@
-from pymusicaltext.core.constants import (
-    BPM_STEP,
-    MIDI_MAXIMUM,
-    MIDI_MINIMUM,
-    VOLUME_DEFAULT,
-)
+from functools import partial
 from random import choice
 from typing import Callable, Dict, List, Union
 
 import mido
+
+from pymusicaltext.core.constants import (
+    BPM_STEP,
+    INSTRUMENT_AGOGO,
+    INSTRUMENT_CHURCH_ORGAN,
+    INSTRUMENT_HARPSICHORD,
+    INSTRUMENT_PAN_FLUTE,
+    INSTRUMENT_TUBULAR_BELLS,
+    MIDI_MAXIMUM,
+    MIDI_MINIMUM,
+    VOLUME_DEFAULT,
+)
 
 from .midiinfo import AdvancedMidiInfo
 from .midiunit import MidiUnit
@@ -24,23 +31,10 @@ class Action(MidiUnit):
         # function that generates the message,
         # return a list for uniformity.
         self.__execute()
-        return_messages = {
-            "bpm+": [
-                mido.MetaMessage(
-                    "set_tempo", tempo=mido.tempo2bpm(self.__info.bpm)
-                )
-            ],
-            "bpm-": [
-                mido.MetaMessage(
-                    "set_tempo", tempo=mido.tempo2bpm(self.__info.bpm)
-                )
-            ],
-            # for some reason, program change is not a MetaMessage, blame mido
-            "\n": [
-                mido.Message("program_change", program=self.__info.instrument)
-            ],
-        }
-        return return_messages.get(self.__action, [])
+        if self.__action in ["?", ".", " "]:
+            # changing octave or changing volume isn't done with messages
+            return []
+        return [mido.Message("program_change", program=self.__info.instrument)]
 
     def __execute(self) -> None:
         """
@@ -55,15 +49,32 @@ class Action(MidiUnit):
         """
         returns the function that the action needs to execute
         """
+        # TODO: a digit is only singular, or
+        # any amount of digits counts as "one digit"?
+        # TODO: "?" and "." means "?.", in sequence,
+        # or at least one of them? As individuals?
         actions: Dict[str, Callable[[], None]] = {
-            "bpm+": self.__increase_bpm,
-            "bpm-": self.__decrease_bpm,
-            "\n": self.__change_instrument,
-            "T+": self.__increase_octave,
-            "T-": self.__decrease_octave,
-            "+": self.__increase_volume,
-            "-": self.__decrease_volume,
+            " ": self.__increase_volume,
+            "?": self.__increase_octave,
+            ".": self.__increase_octave,
+            "!": partial(self.__change_instrument, INSTRUMENT_AGOGO),
+            "O": partial(self.__change_instrument, INSTRUMENT_HARPSICHORD),
+            "o": partial(self.__change_instrument, INSTRUMENT_HARPSICHORD),
+            "I": partial(self.__change_instrument, INSTRUMENT_HARPSICHORD),
+            "i": partial(self.__change_instrument, INSTRUMENT_HARPSICHORD),
+            "U": partial(self.__change_instrument, INSTRUMENT_HARPSICHORD),
+            "u": partial(self.__change_instrument, INSTRUMENT_HARPSICHORD),
+            "\n": partial(self.__change_instrument, INSTRUMENT_TUBULAR_BELLS),
+            ";": partial(self.__change_instrument, INSTRUMENT_PAN_FLUTE),
+            ",": partial(self.__change_instrument, INSTRUMENT_CHURCH_ORGAN),
         }
+        # if the action is none of above, it is a digit
+        # default call defined to avoid too long line
+        if self.__action.isdecimal():
+            return partial(
+                self.__change_instrument,
+                int(self.__action) + self.__info.instrument,
+            )
         return actions[self.__action]
 
     def __increase_octave(self) -> None:
@@ -87,9 +98,9 @@ class Action(MidiUnit):
         # but it resets the volume to the default
         self.__info.volume = VOLUME_DEFAULT
 
-    def __change_instrument(self) -> None:
+    def __change_instrument(self, inst: int) -> None:
         # a random instrument from 0 to 127, as midi standard
-        self.__info.instrument = choice(range(MIDI_MINIMUM, MIDI_MAXIMUM + 1))
+        self.__info.instrument = inst
 
     def test(self) -> None:
         print("Entering Action Test function!")
