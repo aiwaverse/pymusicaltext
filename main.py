05dcc52d24fc1f4b591a1cb5430fbe0c48c4bf4b
Arlen Vasconcelos <arlenvasconcelos@gmail.com>
2021-05-02 01:23:16 -0300

    refactor: gui layout

--- main.py
+++ main.py
@@ -1,46 +1,59 @@
 # %%
 import PySimpleGUI as sg
-
-
+from pymusicaltext.gui.layout import Layout
 # %%
-
-
-def make_gui(title: str) -> sg.Window:
-    sg.theme('DarkBlack1')
-
-    layout = [
-        [sg.Text("Convert Text <-> Music", text_color="#fff", font=("Roboto", 15, "bold"))],
-        [sg.Text("Type a text:", text_color="#fff", font=("Roboto", 11, "bold"))],
-        [
-            sg.Multiline(
-                write_only=True,
-                font=("Ubuntu", 11),
-                size=(40, 10),
-                default_text="Decoded song goes here...",
-                key="-IN-",
-                focus=True,
-            ),
-            sg.Button("Generate music", button_color='#8257e6', border_width=0, font=("Ubuntu", 11), size=(20, 1))
-        ],
-        [sg.Text("Or choose a file:", text_color="#fff", font=("Roboto", 11, "bold"))],
-        [sg.Input(size=(30, 20), font=("Ubuntu", 11)),
-         sg.FileBrowse(button_color='#8257e6', font=("Ubuntu", 11))],
-        [sg.Button("Generate music", button_color='#8257e6', border_width=0, font=("Ubuntu", 11), size=(20, 1))],
-        [sg.Exit(button_color='#dc3545')],
-    ]
-    return sg.Window(title, layout)
+from pymusicaltext import Player
 
 
 def main() -> None:
-    gui = make_gui("PyMusicalText")
-    # a_player = Player("aaaaaaaaaaa", "file.mid", )
+    layout = Layout("PyMusicalText")
+    window = layout.make_gui()
+
+    # TODO: remove dead code
+    # progress = 0
+    # progress_bar = window['progressbar']
     while True:
-        event, values = gui.read()
+        event, values = window.read()
         if event == sg.WIN_CLOSED or event == "Exit":
             break
         if event == "Generate music":
-            gui["-OUT-"].update(values["-IN-"])
-    gui.close()
+            print(values)
+            a_player = Player(values['in'], "file.mid")
+            sg.popup('Musica Gerada com sucesso')
+
+        # if progress < 1000:
+        #     print(progress)
+        #     progress += 10
+        #     progress_bar.UpdateBar(progress)
+
+        # if event == 'play':
+        #     list_player.play()
+        # if event == 'pause':
+        #     list_player.pause()
+        # if event == 'stop':
+        #     list_player.stop()
+        # if event == 'next':
+        #     list_player.next()
+        #     list_player.play()
+        # if event == 'previous':
+        #     list_player.previous()  # first call causes current video to start over
+        #     list_player.previous()  # second call moves back 1 video from current
+        #     list_player.play()
+        # if event == 'load':
+        #     if values['-VIDEO_LOCATION-'] and not 'Video URL' in values['-VIDEO_LOCATION-']:
+        #         media_list.add_media(values['-VIDEO_LOCATION-'])
+        #         list_player.set_media_list(media_list)
+        #         window['-VIDEO_LOCATION-'].update('Video URL or Local Path:')  # only add a legit submit
+
+        # update elapsed time if there is a video loaded and the player is playing
+        # if player.is_playing():
+        #     window['-MESSAGE_AREA-'].update(
+        #         "{:02d}:{:02d} / {:02d}:{:02d}".format(*divmod(player.get_time() // 1000, 60),
+        #                                                *divmod(player.get_length() // 1000, 60)))
+        # else:
+        #     window['-MESSAGE_AREA-'].update('Load media to start' if media_list.count() == 0 else 'Ready to play media')
+
+    window.close()
 
 
 if __name__ == "__main__":
