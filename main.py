b710d4aa47548e04a3a10c835f0e28e8f369c5f6
aiwaverse <aiwavision@protonmail.com>
2021-05-15 11:16:36 -0300

    Linted, used constants, added wrong file name hand

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
-# %%
-from datetime import date, time
-from pymusicaltext.gui.constants import VOLUME_CHANGE
-
-import mido
+"""
+The main module of PyMusicalText, use it to run the
+program as a graphical interactive interface
+"""
 import PySimpleGUI as sg
 
+from pymusicaltext.gui.constants import (
+    DOWNLOAD_BUTTON,
+    GENERATE_MUSIC,
+    PAUSE,
+    START,
+    STOP,
+    VOLUME_CHANGE,
+)
 from pymusicaltext.gui.interface import GUI
 
 
 def main() -> None:
+    """
+    Main function
+    """
     program_gui = GUI("PyMusicalText")
     while True:
         event, values = program_gui.read(timeout=1000)
-        if event == "Gerar música" and not program_gui.empty_input():
-            program_gui.create_player()
-            sg.popup("Musica Gerada com sucesso")
-            program_gui.make_full_gui_visible()
-        if event == "Start":
+        if event == GENERATE_MUSIC and not program_gui.empty_input():
+            try:
+                program_gui.create_player()
+                sg.popup("Musica Gerada com sucesso")
+                program_gui.make_full_gui_visible()
+            except ValueError:
+                continue
+        if event == START:
             program_gui.start_song()
-        if event == "Pause":
+        if event == PAUSE:
             program_gui.pause_song()
-        if event == "Stop":
+        if event == STOP:
             program_gui.stop_song()
-        if event == "Baixar":
+        if event == DOWNLOAD_BUTTON:
             selected_folder = sg.popup_get_folder(
                 "Por favor, entre com a pasta de destino",
                 title="Baixar arquivo",
@@ -31,14 +44,12 @@ def main() -> None:
                 program_gui.download_file(selected_folder)
         if event == VOLUME_CHANGE:
             program_gui.change_volume(values[VOLUME_CHANGE])
-        if event == sg.WIN_CLOSED or event == "Exit":
+        if event in (sg.WIN_CLOSED, "Exit"):
             program_gui.close_player()
             break
         if program_gui.playing:
             program_gui.update_progress_bar()
 
-
-# %%
 
 if __name__ == "__main__":
     main()
